d4701d18d9aa4fae419f9e862a118982e9b8fdb6
Andrey Turpanov <turpanov.a@gmail.com>
2022-07-25 15:20:53 +0300

    test

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -18,8 +18,7 @@ class StaticPagesURLTests(TestCase):
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
         response = self.guest_client.get('/')
-        self.assertContains(
-            response.content.decode('utf-8'), 'У меня получилось!')
+        self.assertContains(response.content.decode('utf-8'), 'У меня получилось!')
 
         response = self.guest_client.get('/second_page/')
         self.assertContains(
